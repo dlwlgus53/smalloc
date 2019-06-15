735804bd031fed5f0b07e6261f81e61d728c4fcd
dlwlgus53 <dlwlgus4553@gmail.com>
2019-06-15 14:44:05 +0900

    test4.c

---
+++ smalloc-1.0/test4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "smalloc.h"
+
+int 
+main()
+{
+	void *p1, *p2, *p3, *p4 ;
+
+	print_sm_containers() ;
+
+	p1 = smalloc(5000) ; 
+	printf("smalloc(5000)\n") ; 
+	print_sm_containers() ;
+
+	p2 = smalloc(3500) ; 
+	printf("smalloc(3500)\n") ; 
+	print_sm_containers() ;
+
+	p3 = smalloc(10) ; 
+	printf("smalloc(10)\n") ; 
+	print_sm_containers() ;
+
+    	print_sm_uses() ; 
+}
