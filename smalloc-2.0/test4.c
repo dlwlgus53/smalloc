970afcd8cb5794f7341381849b1142eba8268778
dlwlgus53 <dlwlgus4553@gmail.com>
2019-06-15 16:06:41 +0900

    unused list

---
+++ smalloc-2.0/test4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "smalloc.h"
+
+int 
+main()
+{
+	void *p1, *p2, *p3, *p4 ;
+
+	print_sm_containers() ;
+
+	p1 = smalloc(5000) ; 
+	printf("smalloc(5000)\n") ; 
+	print_sm_containers() ;
+	print_unused();
+	
+	p2 = smalloc(3500) ; 
+	printf("smalloc(3500)\n") ; 
+	print_sm_containers() ;
+	print_unused();
+
+	p3 = smalloc(10) ; 
+	printf("smalloc(10)\n") ; 
+	print_sm_containers() ;
+
+    	print_sm_uses() ; 
+	print_unused() ;
+}
